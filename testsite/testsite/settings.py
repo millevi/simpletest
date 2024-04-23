814ff96f76133ea6f2f84aba6b043c7cf9e6867d
alex <alex@test.loc>
2024-04-23 22:32:06 +0300

    Путь к БД

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'testsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbase' / 'db.sqlite3',
     }
 }
 
