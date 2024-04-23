ab1e9c99d6d575d9e720f1a63a36af03d971cac4
Konstantin Gurashkin <125302139+CrockoMan@users.noreply.github.com>
2024-04-23 22:57:55 +0300

    Bot (#4)
    * add one
    
    * crate app bot
    
    * gitignor

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'bot',
 ]
 
 MIDDLEWARE = [
